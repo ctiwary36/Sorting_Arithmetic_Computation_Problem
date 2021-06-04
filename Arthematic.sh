2e671d88da6c708e39b28ba604d0bf3a7166f552
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-04 21:48:30 +0530

    Added:Use_Case1_Taking_Three_Input

--- Arthematic.sh
+++ Arthematic.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x
 echo "Arithmetic Compution And Sorting"
+
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
