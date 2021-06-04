70ced31212aeaa68703a3b5a0bd83ec04c483a0d
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-04 21:59:34 +0530

    Added:

--- Arthematic.sh
+++ Arthematic.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 echo "Arithmetic Compution And Sorting"
 
-read -p "Enter first input :" firstInput
-read -p "Enter second input:" secondInput
-read -p "Enter third input :" thirdInput
+read -p "Enter first input :" a
+read -p "Enter second input:" b
+read -p "Enter third input :" c
+
+result=$(( a+(b*c) )) 
+echo "Result:" $result
